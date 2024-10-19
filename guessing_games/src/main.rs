f0cd2d200e2f9e9705b22290ed7b5a062186227a
lemon_42 <etclegg96@gmail.com>
2024-10-19 17:22:49 +0200

    Add basic template for guessing games commented

--- guessing_games/src/main.rs
+++ guessing_games/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number !");
+
+    println!("Please input your guess.");
+
+    // Initialize an empty string to store the guess
+    let mut guess = String::new();
+
+    // Reads input from stdin and store it in the mutable variable guess
+    // The `read_line()` require a mutable reference (`&`) so it can modify the variable
+    // Since the `read_line()` return a `Result`, we handle any potential error using `expect()`
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed : {}", guess);
 }
