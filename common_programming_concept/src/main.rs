8bfdf48249010e0ef3860ef824a4b01e7e3ea76d
lemon_42 <etclegg96@gmail.com>
2024-10-19 21:47:56 +0200

    Remove unnecessary lines

--- common_programming_concept/src/main.rs
+++ common_programming_concept/src/main.rs
@@ -7,9 +7,7 @@ fn main() {
     println!("Let's learn more about common programming concept!\n");
 
     // The following is used to not have compilation error
-    println!("More about variables.\n");
     variables::variables();
 
-    println!("\nMore about data types.\n");
     data_types::data_types();
 }
