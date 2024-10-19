26b25bd6c36df9d07fcd44e2030c00cbda06c1f5
lemon_42 <etclegg96@gmail.com>
2024-10-19 20:17:43 +0200

    Mod data_types file, use code in variables.rs to not get error

--- common_programming_concept/src/main.rs
+++ common_programming_concept/src/main.rs
@@ -1,5 +1,15 @@
+// Mod file to have rust-analyzer working on those file
+// More about this later
 mod variables;
+mod data_types;
 
 fn main() {
-    println!("Let's learn more about common programming concept!");
+    println!("Let's learn more about common programming concept!\n");
+
+    // The following is used to not have compilation error
+    println!("More about variables.\n");
+    variables::variables();
+
+    println!("\nMore about data types.\n");
+    data_types::data_types();
 }
