c8752d66407eaa72ace74980ce6c9582396d7443
lemon_42 <etclegg96@gmail.com>
2024-10-19 23:14:47 +0200

    Add attribute to allow unused variable for clearer code

--- common_programming_concept/src/data_types_scalar.rs
+++ common_programming_concept/src/data_types_scalar.rs
@@ -1,3 +1,5 @@
+#[allow(unused_variables)]
+
 pub fn scalar() {
     /*          Integers Types
      *
@@ -14,12 +16,12 @@ pub fn scalar() {
      */
 
     // We can write this to specify the type
-    let _forty_two = 42u8;
-    let _decimal = 98_222;
-    let _hexadecimal = 0xff;
-    let _octal = 0o77;
-    let _binary = 0b1111_0000;
-    let _octal_two = b'A'; // u8 only
+    let forty_two = 42u8;
+    let decimal = 98_222;
+    let hexadecimal = 0xff;
+    let octal = 0o77;
+    let binary = 0b1111_0000;
+    let octal_two = b'A'; // u8 only
 
     // Uncommenting the following lines will result in a compilation error
     // Due to buffer and under overflow
@@ -29,22 +31,22 @@ pub fn scalar() {
 
     /*          Float types              */
 
-    let _x = 2.0;
-    let _y: f32 = 3.0;
+    let x = 2.0;
+    let y: f32 = 3.0;
 
-    /*          Numeric Operation        */
-    let _sum = 5 + 10;
-    let _difference = 95.5 - 4.3;
-    let _product = 4 * 30;
-    let _quotient = 56.7 / 32.2;
-    let _truncated = -5 / 3; // Results in -1
-    let _remainder = 43 % 5;
+    /*         Numeric Operation        */
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+    let remainder = 43 % 5;
 
     /*          Boolean types            */
-    let _t = true;
-    let _f: bool = false;
+    let t = true;
+    let f: bool = false;
 
-    /*          Character types          */
-    let _c = 'z';
-    let _penguin = '🐧';
+    /*         Character types          */
+    let c = 'z';
+    let penguin = '🐧';
 }
