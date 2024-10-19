a9933d8fbe7e90651204c5fd87fcb57dd1f87b62
lemon_42 <etclegg96@gmail.com>
2024-10-19 23:06:57 +0200

    Update main file for data_types mod and used function

--- common_programming_concept/src/main.rs
+++ common_programming_concept/src/main.rs
@@ -1,7 +1,7 @@
 // Mod file to have rust-analyzer working on those file
 // More about this later
 mod variables;
-mod data_types;
+mod data_types_scalar;
 
 fn main() {
     println!("Let's learn more about common programming concept!\n");
@@ -9,5 +9,5 @@ fn main() {
     // The following is used to not have compilation error
     variables::variables();
 
-    data_types::data_types();
+    data_types_scalar::scalar();
 }
