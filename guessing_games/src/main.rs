dd778f97faadbd928341a5cd64fd099e3592b8e8
lemon_42 <etclegg96@gmail.com>
2024-10-19 18:09:43 +0200

    Move game into a loop, add better error handling with match

--- guessing_games/src/main.rs
+++ guessing_games/src/main.rs
@@ -10,29 +10,38 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Initialize an empty string to store the guess
-    let mut guess = String::new();
+        // Initialize an empty string to store the guess
+        let mut guess = String::new();
 
-    // Reads input from stdin and store it in the mutable variable guess
-    // The `read_line()` require a mutable reference (`&`) so it can modify the variable
-    // Since the `read_line()` return a `Result`, we handle any potential error using `expect()`
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+        // Reads input from stdin and store it in the mutable variable guess
+        // The `read_line()` require a mutable reference (`&`) so it can modify the variable
+        // Since the `read_line()` return a `Result`, we handle any potential error using `expect()`
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    // Shadow the old `guess` variable with this new one
-    // First, we use `trim()` to remove all trailing character and whitespaces
-    // Then we parse it into another type, here u32 to match with `secret_number` variable
-    // As `parse()` return a `Result`,we handle any potential error using `expect()`
-    let guess = guess.trim().parse::<u32>().expect("Please enter a number !");
+        // Shadow the old `guess` variable with this new one
+        // First, we use `trim()` to remove all trailing character and whitespaces
+        // Then we attempt to parse the string into a `u32` to match the `secret_number` variable
+        // Since `parse()` returns a `Result`, we handle potential errors using `match`
+        // If parsing is successful, we store the value in `number`. If it fails, we skip the current iteration with `continue`
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
 
-    println!("You guessed : {}", guess);
+        println!("You guessed : {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("It's more !"),
-        Ordering::Greater => println!("It's less !"),
-        Ordering::Equal => println!("You win !"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("It's more !"),
+            Ordering::Greater => println!("It's less !"),
+            Ordering::Equal => {
+                println!("You win !");
+                break;
+            }
+        }
     }
 }
