d63c04b7a395b564da06b17fadf16c9e370b5e2a
lemon_42 <etclegg96@gmail.com>
2024-10-20 15:46:34 +0200

    Add working function for the fibonacci sequence

--- n_number_of_fibonacci/src/main.rs
+++ n_number_of_fibonacci/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+fn fibonacci(n: u32) -> u32 {
+    if n < 2 {
+        return n;
+    }
+
+    fibonacci(n - 1) + fibonacci(n - 2)
 }
+
+fn main() {
+    let mut number = 1;
+
+    while number <= 15 {
+        println!("Fibonacci({number}) is {}", fibonacci(number));
+        number += 1;
+    }
+}
+
