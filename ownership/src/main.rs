421f8f22b6f6280b3f443d0f1114e3869dd7dae7
lemon_42 <etclegg96@gmail.com>
2024-10-20 22:19:29 +0200

    Use rustfmt to format the code

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -33,7 +33,7 @@ fn main() {
     }                      // this scope is now over, and s is no longer valid
     */
 
-    // Uncommenting the following line result in an error. 
+    // Uncommenting the following line result in an error.
     // Since both variables would point to the same memory address, Rust prevents a double free error.
     // When `s1` goes out of scope, it would try to free the same memory twice, leading to memory safety issues.
     // let s1 = String::from("hello");
@@ -46,13 +46,13 @@ fn main() {
     println!("{s1} | {s2}");
 
     // The following implement the `Copy` trait. This means that a variable is always effective
-    // after being assigned to another variable (no need to clone) : 
+    // after being assigned to another variable (no need to clone) :
     //
     // All `integer` type
     // Boolean type
     // Floating-point types
     // Character type
-    // Tuple, only if they contain type that implement the `Copy` trait. 
+    // Tuple, only if they contain type that implement the `Copy` trait.
     // So (i32, i32) this works and this (i32, String) doesn't.
 
     // Uncommenting the following line result in an error.
